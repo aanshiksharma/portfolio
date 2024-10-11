71f813b76c30d2089aad7e3c684b36750a8ad126
Aanshik <aanshik16@gmail.com>
2024-10-11 10:52:52 +0530

    Edited the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,11 +8,18 @@ function Navbar() {
     <>
       <header>
         <div className="wrapper-1 flex">
-          <div className="logo">Aanshik Sharma</div>
+          <div className="logo flex">
+            <div className="profileLogo">
+              <img src="img/profileLogo.jpg" alt="" />
+            </div>
+            <p className="logoText">Aanshik Sharma</p>
+          </div>
           <nav className="navbar">
             <ul className="navlist flex">
               <li>
-                <Link to="/" className="selected" >Portfolio</Link>
+                <Link to="/" className="selected">
+                  Portfolio
+                </Link>
               </li>
               <li>
                 <Link to="/projects">Projects</Link>
@@ -22,6 +29,15 @@ function Navbar() {
               </li>
               <li>
                 <Link to="/contact">Contact</Link>
+              </li>
+              <li>
+                <a
+                  href="./aanshik_sharma__resume.pdf"
+                  download="aanshik_sharma__resume.pdf"
+                  className="text-cta"
+                >
+                  Download Resume
+                </a>
               </li>
             </ul>
           </nav>
