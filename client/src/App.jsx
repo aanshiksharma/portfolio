1b7028743f765636657240e86154cccb73e5f8d5
Aanshik Sharma <aanshik16@gmail.com>
2025-08-18 02:26:39 +0530

    Minor changes in app.jsx corresponding to app page changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router";
+import { gsap } from "gsap";
 
 // Style
 import "./app.css";
@@ -9,23 +10,37 @@ import ProjectPage from "./pages/ProjectPage";
 // Pages or Routes (whatever you wanna call them)
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Contact from "./pages/Contact";
 import Projects from "./pages/Projects";
 import NoPage from "./pages/NoPage";
 import Blog from "./pages/Blog";
 import Terminal from "./pages/Terminal";
 
 function App() {
+  document.getElementById("root").addEventListener("mousemove", (dets) => {
+    // gsap.to("#custom-cursor", {
+    //   x: dets.x - 6,
+    //   y: dets.y - 6,
+    //   duration: 0.5,
+    // });
+  });
+
   return (
     <>
+      <div
+        id="custom-cursor"
+        className="w-3 h-3 rounded-full
+        fixed top-0 left-0 z-1
+        bg-text-base
+        "
+      ></div>
+
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
           <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
           <Route path="/terminal" element={<Terminal />} />
           <Route path="/blog" element={<Blog />} />
-          <Route path="/contact" element={<Contact />} />
 
           {/* This path needs a new name */}
           <Route
