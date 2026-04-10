0f4d427f1bd6872baa32c8e26a25bc01fad234e0
Aanshik Sharma <aanshik16@gmail.com>
2026-04-10 19:59:05 +0530

    Allowed network for dev server

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   images: {
     remotePatterns: [
@@ -8,6 +9,7 @@ const nextConfig = {
       },
     ],
   },
+  allowedDevOrigins: ["192.168.1.6"],
 };
 
 export default nextConfig;
