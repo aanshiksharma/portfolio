e4960d7861975895ab15710b4d4a1100ed83f84d
Aanshik Sharma <aanshik16@gmail.com>
2026-06-13 08:39:16 +0530

    Added a hook to get data and now loading dynamic data from cms

--- app/_components/ProjectsSection.jsx
+++ app/_components/ProjectsSection.jsx
@@ -1,14 +1,36 @@
+import { useEffect, useState } from "react";
+
 import {
   useBackgroundAnimation,
   useHeadingAnimation,
 } from "../hooks/projectAnimations";
 
 import ProjectCard from "./ProjectsSection/ProjectCard";
-import projects from "@/data/projects.json";
+// import projects from "@/data/projects.json";
 
 function ProjectsSection() {
+  const [projects, setProjects] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
+  console.log(BACKEND_URL);
   useBackgroundAnimation();
   useHeadingAnimation();
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      try {
+        const res = await fetch(`${BACKEND_URL}/api/projects`);
+        const data = await res.json();
+        setProjects(data);
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchProjects();
+  }, []);
 
   return (
     <section id="projects" className="">
@@ -20,9 +42,15 @@ function ProjectsSection() {
       </div>
 
       <div className="flex flex-col gap-15">
-        {projects.map((project) => (
-          <ProjectCard key={project._id.$oid} project={project} />
-        ))}
+        {loading ? (
+          <h4 className="text-2xl text-center text-background">
+            Loading Projects...
+          </h4>
+        ) : (
+          projects.map((project) => (
+            <ProjectCard key={project._id} project={project} />
+          ))
+        )}
       </div>
     </section>
   );
