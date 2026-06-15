ab3acd4c0ed6ed00f10260fcf9b6824db3345ca8
Aanshik Sharma <aanshik16@gmail.com>
2026-06-15 13:44:22 +0530

    Minor bug fixes

--- app/_components/ProjectsSection.jsx
+++ app/_components/ProjectsSection.jsx
@@ -1,9 +1,6 @@
 import { useEffect, useState } from "react";
 
-import {
-  useBackgroundAnimation,
-  useHeadingAnimation,
-} from "../hooks/projectAnimations";
+import { useHeadingAnimation } from "../hooks/projectAnimations";
 
 import ProjectCard from "./ProjectsSection/ProjectCard";
 import projects from "@/data/projects.json";
@@ -32,18 +29,16 @@ function ProjectsSection() {
 
   return (
     <section id="projects">
-      <div className="sticky -top-1/3 h-screen flex items-center justify-center bg-background overflow-hidden">
-        <h2 className="text-[9dvw] text-center font-semibold tracking-tight uppercase flex items-center gap-2 md:gap-6">
+      <div className="sticky -top-1/8 md:-top-1/3 h-[60vh] md:h-screen flex items-center justify-center bg-background overflow-hidden">
+        <h2 className="text-[15vw] md:text-[9vw] text-center font-semibold tracking-tight uppercase flex max-md:flex-col items-center md:gap-6">
           <span className="fromLeft relative">Featured</span>
           <span className="fromRight relative">Projects</span>
         </h2>
       </div>
 
-      <div className="relative z-1 flex flex-col bg-dark-background px-4 lg:px-12">
+      <div className="relative z-1 flex flex-col bg-dark-background px-4 lg:px-12 max-lg:py-20 max-lg:gap-20">
         {loading ? (
-          <h4 className="text-2xl text-center text-dark-foreground">
-            Loading Projects...
-          </h4>
+          <p>Loading Projects...</p>
         ) : (
           projects.map((project, index) => (
             <ProjectCard
--- app/_components/SkillsSection.jsx
+++ app/_components/SkillsSection.jsx
@@ -2,9 +2,12 @@ import SkillsData from "./SkillsSection/SkillsData";
 
 function SkillsSection() {
   return (
-    <section id="skills" className="relative bg-dark-background">
+    <section
+      id="skills"
+      className="relative bg-dark-background px-4 py-10 flex items-center justify-center min-h-screen"
+    >
       <div className="absolute inset-0 grid items-center justify-center">
-        <h2 className="uppercase bg-linear-to-b from-dark-foreground/45 to-dark-foreground/5 bg-clip-text text-transparent text-center text-[10vw] font-semibold">
+        <h2 className="uppercase bg-linear-to-b from-dark-foreground/45 to-dark-foreground/5 bg-clip-text text-transparent text-center lg:text-[9vw] text-[15vw] font-semibold">
           My tech stack
         </h2>
       </div>
