c0bbb84501547302d1492e5ec0b231c54c599645
Aanshik Sharma <aanshik16@gmail.com>
2026-06-21 16:28:52 +0530

    Project card animations done

--- app/hooks/projectAnimations.js
+++ app/hooks/projectAnimations.js
@@ -29,3 +29,62 @@ export const useHeadingAnimation = () => {
     );
   }, []);
 };
+
+export const useProjectCardAnimation = () => {
+  useGSAP(() => {
+    const mm = gsap.matchMedia();
+
+    mm.add("(hover: hover)", () => {
+      createAnimations("top 30%");
+    });
+
+    mm.add("(hover: none)", () => {
+      createAnimations("top 60%");
+    });
+
+    function createAnimations(start) {
+      const projectCards = gsap.utils.toArray(".projectCard");
+
+      projectCards.map((project) => {
+        const timeline = gsap.timeline({
+          paused: true,
+        });
+
+        timeline
+          .fromTo(
+            project.querySelector(".fromLeft"),
+            {
+              xPercent: -25,
+              opacity: 0,
+            },
+            {
+              xPercent: 0,
+              opacity: 1,
+              duration: 1,
+              ease: "power2.out",
+            },
+          )
+          .fromTo(
+            project.querySelector(".fromRight"),
+            { xPercent: 25, opacity: 0 },
+            {
+              xPercent: 0,
+              opacity: 1,
+              duration: 1,
+              ease: "power2.out",
+            },
+            "<",
+          );
+
+        ScrollTrigger.create({
+          trigger: project,
+          start,
+          onEnter: () => timeline.play(),
+          onLeaveBack: () => timeline.reverse(),
+        });
+      });
+    }
+
+    return () => mm.revert();
+  }, []);
+};
