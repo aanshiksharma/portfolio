29202c5969e7be9e9a128a34918ce37196a8a806
Aanshik Sharma <aanshik16@gmail.com>
2026-06-21 21:43:22 +0530

    Footer animation bug fix

--- app/hooks/useFooterAnimation.js
+++ app/hooks/useFooterAnimation.js
@@ -5,75 +5,65 @@ import ScrollTrigger from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
 export const useFooterAnimation = () => {
-  function footerTopAnimation() {
-    useGSAP(() => {
-      const timeline = gsap.timeline({ paused: true });
+  useGSAP(() => {
+    const timeline = gsap.timeline({ paused: true });
 
-      timeline
-        .from(".footer-top h2", {
-          xPercent: -25,
-          opacity: 0,
-        })
-        .from(".footer-top .top-button", {
-          xPercent: 25,
-          opacity: 0,
-        });
-
-      ScrollTrigger.create({
-        trigger: ".footer-top",
-        scroller: "body",
-        start: "top 70%",
-        onEnter: () => timeline.play(),
-        onLeaveBack: () => timeline.reverse(),
-      });
-    }, []);
-  }
-
-  function linksAnimation() {
-    useGSAP(() => {
-      const timeline = gsap.timeline({ paused: true });
-
-      timeline.from(".links > *", {
+    timeline
+      .from(".footer-top h2", {
+        xPercent: -25,
         opacity: 0,
-        yPercent: 25,
-        duration: 0.3,
-        stagger: 0.15,
-        ease: "power1.out",
-      });
-
-      ScrollTrigger.create({
-        trigger: ".links",
-        scroller: "body",
-        start: "top 80%",
-        onEnter: () => timeline.play(),
-        onLeaveBack: () => timeline.reverse(),
-      });
-    }, []);
-  }
-
-  function logoAnimation() {
-    useGSAP(() => {
-      const timeline = gsap.timeline({ paused: true });
-
-      timeline.from(".footer-logo > *", {
+      })
+      .from(".footer-top .top-button", {
+        xPercent: 25,
         opacity: 0,
-        yPercent: 25,
-        duration: 1,
-        stagger: 0.5,
-        ease: "power1.out",
       });
 
-      ScrollTrigger.create({
-        trigger: ".footer-logo",
-        scroller: "body",
-        start: "top 90%",
-        onEnter: () => timeline.play(),
-        onLeaveBack: () => timeline.reverse(),
-      });
-    }, []);
-  }
+    ScrollTrigger.create({
+      trigger: ".footer-top",
+      scroller: "body",
+      start: "top 70%",
+      onEnter: () => timeline.play(),
+      onLeaveBack: () => timeline.reverse(),
+    });
+  }, []);
 
-  footerTopAnimation();
-  linksAnimation();
-  logoAnimation();
+  useGSAP(() => {
+    const timeline = gsap.timeline({ paused: true });
+
+    timeline.from(".links > *", {
+      opacity: 0,
+      yPercent: 25,
+      duration: 0.3,
+      stagger: 0.15,
+      ease: "power1.out",
+    });
+
+    ScrollTrigger.create({
+      trigger: ".links",
+      scroller: "body",
+      start: "top 80%",
+      onEnter: () => timeline.play(),
+      onLeaveBack: () => timeline.reverse(),
+    });
+  }, []);
+
+  useGSAP(() => {
+    const timeline = gsap.timeline({ paused: true });
+
+    timeline.from(".footer-logo > *", {
+      opacity: 0,
+      yPercent: 25,
+      duration: 1,
+      stagger: 0.5,
+      ease: "power1.out",
+    });
+
+    ScrollTrigger.create({
+      trigger: ".footer-logo",
+      scroller: "body",
+      start: "top 90%",
+      onEnter: () => timeline.play(),
+      onLeaveBack: () => timeline.reverse(),
+    });
+  }, []);
 };
